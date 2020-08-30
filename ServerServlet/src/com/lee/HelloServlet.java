45f99c45266894f86e96afe8de43267e186cb5ab
NoobYang <liyang@lideMacBook-Pro.local>
2020-08-30 12:10:09 +0800

    web lesson two: hello servlet

---
+++ ServerServlet/src/com/lee/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.lee;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        // 初始化
+        message = "Hello, First Servlet!";
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置:响应内容类型
+        response.setContentType("text/html");
+
+        // 输出文本
+        PrintWriter out = response.getWriter();
+        out.write("<h1> " + message + " </h1>");
+    }
+
+}
