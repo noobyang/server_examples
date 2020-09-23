a0d6736e9c4cf436c80e5d7598a19d6e135ee9d3
1234567890 <1234567890>
2020-09-23 16:09:47 +0800

    web lesson three:struts2 study

--- SimpleStruct/src/main/java/com/noobyang/PrivilegeAction.java
+++ SimpleStruct/src/main/java/com/noobyang/PrivilegeAction.java
@@ -4,6 +4,14 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class PrivilegeAction extends ActionSupport {
 
+    // 如果我们 action 不写 method 的话，默认执行的是 execute() 方法, execute() 方法默认返回值是 SUCCESS
+    @Override
+    public String execute() throws Exception {
+        System.out.println("我是execute()");
+
+        return SUCCESS;
+    }
+
     public String login() {
         System.out.println("我是登陆");
 
