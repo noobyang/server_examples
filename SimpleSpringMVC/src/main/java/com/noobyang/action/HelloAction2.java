7d49a262ba3d404574759a53922f8ea5554ac963
1234567890 <1234567890>
2020-10-27 15:45:55 +0800

    web lesson three:config SimpleUrlHandlerMapping

--- SimpleSpringMVC/src/main/java/com/noobyang/action/HelloAction2.java
+++ SimpleSpringMVC/src/main/java/com/noobyang/action/HelloAction2.java
@@ -3,17 +3,17 @@ package com.noobyang.action;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloAction extends AbstractController {
+public class HelloAction2 implements Controller {
 
-    private static final Logger log = LoggerFactory.getLogger(HelloAction.class);
+    private static final Logger log = LoggerFactory.getLogger(HelloAction2.class);
 
     @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
 
         log.info("handleRequest");
