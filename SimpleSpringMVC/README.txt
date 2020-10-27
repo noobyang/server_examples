ec5518baa73105a09457951981c26efc4d8453d0
1234567890 <1234567890>
2020-10-27 15:38:08 +0800

    web lesson three:config SimpleUrlHandlerMapping

--- SimpleSpringMVC/README.txt
+++ SimpleSpringMVC/README.txt
@@ -3,6 +3,31 @@
    applicationContext 的路径 spring 默认会到 WEB-INF/ 下，
    手动修改为 resources 下需要修改 web.xml 中的配置为 classpath:applicationContext.xml。
 
+2. SpringMVC 工作流程
+   用户发送请求
+   请求交由核心控制器处理
+   核心控制器找到映射器，映射器看看请求路径是什么
+   核心控制器再找到适配器，看看有哪些类实现了Controller接口或者对应的bean对象
+   将带过来的数据进行转换，格式化等等操作
+   找到我们的控制器Action，处理完业务之后返回SimpleUrlHandlerMapping一个ModelAndView对象
+   最后通过视图解析器来对ModelAndView进行解析
+   跳转到对应的JSP/html页面
+
+3. 映射器
+   只要是.action 为后缀的请求都是会经过SpringMVC的核心Servlet
+   org.springframework.web.servlet.handler.SimpleUrlHandlerMapping
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- SimpleSpringMVC/src/main/java/com/noobyang/action/HelloAction.java
+++ SimpleSpringMVC/src/main/java/com/noobyang/action/HelloAction.java
@@ -3,14 +3,18 @@ package com.noobyang.action;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class HelloAction implements Controller {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloAction extends AbstractController {
 
     private static final Logger log = LoggerFactory.getLogger(HelloAction.class);
 
     @Override
-    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
 
         log.info("handleRequest");
