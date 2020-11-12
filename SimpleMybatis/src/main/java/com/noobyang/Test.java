8f7a6ca36f1e355e24fd201edb3392c02f7d40ca
liyang <1234567890>
2020-11-12 13:46:27 +0800

    web lesson two:insert data, cannot auto create table

--- SimpleMybatis/src/main/java/com/noobyang/Test.java
+++ SimpleMybatis/src/main/java/com/noobyang/Test.java
@@ -1,10 +1,37 @@
 package com.noobyang;
 
+import com.noobyang.entity.Student;
+import org.apache.ibatis.session.SqlSession;
+
 public class Test {
 
     public static void main(String[] args) {
 
+        addStudent();
+
 
     }
+
+    public static void addStudent() {
+        Student student = new Student(1, "noobyang", 10000d);
+        // 得到连接对象
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        try {
+            // 映射文件的命名空间.SQL片段的ID，就可以调用对应的映射文件中的SQL
+            sqlSession.insert("add", student);
+            sqlSession.commit();
+
+            Student user = sqlSession.selectOne("findById", 1);
+            System.out.println(user.getName());
+        } catch (Exception e){
+            e.printStackTrace();
+            sqlSession.rollback();
+            throw e;
+        } finally {
+            MybatisUtil.closeSqlSession();
+        }
+
+    }
+
 
 }
--- SimpleMybatis/src/main/java/com/noobyang/entity/Student.java
+++ SimpleMybatis/src/main/java/com/noobyang/entity/Student.java
@@ -6,7 +6,10 @@ public class Student {
     private String name;
     private Double sal;
 
-    public Student() {
+    public Student(Integer id, String name, Double sal) {
+        this.id = id;
+        this.name = name;
+        this.sal = sal;
     }
 
     public Integer getId() {
