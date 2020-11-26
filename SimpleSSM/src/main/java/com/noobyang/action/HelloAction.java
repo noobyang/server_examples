53d26d71c89117b796d11eb5e578f574360a8034
NoobYang <liyang@lideMacBook-Pro.local>
2020-11-26 21:41:56 +0800

    lesson ssm: action assess, usually rebuild

--- SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
+++ SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
@@ -1,39 +1,27 @@
 package com.noobyang.action;
 
-import com.noobyang.dao.impl.DeptDaoImpl;
-import com.noobyang.entity.Dept;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.annotation.Resource;
-import java.util.Date;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloAction {
 
     private static final Logger logger = LogManager.getLogger("HelloAction");
 
-//    @Resource
-//    private DeptDaoImpl deptDao;
-
-    @RequestMapping(value="/insert.action", method = RequestMethod.GET)
-    public String insert(){
-        logger.debug("insert ");
-//        deptDao.insertDept(dept);
-
+    @RequestMapping(value="/params2.action", method = RequestMethod.POST)
+    public String params2(Model model, String username, int[] hobby) throws Exception {
+        String str = "";
+        for (int i : hobby) {
+            str  = str + i + " ";
+        }
+        logger.info("params2 " + username + " " + str);
+        model.addAttribute("message", username + " " + str);
         return "message";
     }
 
-    @RequestMapping(value="/params6.action", method = RequestMethod.POST)
-    public String params6(Model model) throws Exception {
-        logger.debug("params6 ");
 
-        model.addAttribute("message", "redirect");
-        return "message";
-    }
 
 }
--- SimpleSSM/src/main/webapp/insert.jsp
+++ SimpleSSM/src/main/webapp/insert.jsp
@@ -1,20 +1,36 @@
 <%--
   Created by IntelliJ IDEA.
   User: Administrator
-  Date: 2020/11/13
-  Time: 16:28
+  Date: 2020/10/26
+  Time: 16:27
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Insert</title>
+    <title>Post</title>
 </head>
 <body>
+<form action="/params2.action" method="post">
+    <table align="center">
+        <tr>
+            <td>用户名：</td>
+            <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+            <td>爱好</td>
+            <td><input type="checkbox" name="hobby" value="1">篮球</td>
+            <td><input type="checkbox" name="hobby" value="2">足球</td>
+            <td><input type="checkbox" name="hobby" value="3">排球</td>
+            <td><input type="checkbox" name="hobby" value="4">羽毛球</td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input type="submit" value="提交">
+            </td>
+        </tr>
+    </table>
 
-<form action="params6.action" method="post">
-    <input type="submit" value="提交">
 </form>
-
 </body>
 </html>
