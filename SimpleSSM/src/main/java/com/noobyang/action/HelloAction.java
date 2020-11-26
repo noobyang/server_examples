87763e7820c9ffcfe3f6c99015c024939a4d71e5
NoobYang <liyang@lideMacBook-Pro.local>
2020-11-26 22:15:28 +0800

    lesson ssm: action assess, usually delete target

--- SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
+++ SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
@@ -3,7 +3,6 @@ package com.noobyang.action;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -12,16 +11,9 @@ public class HelloAction {
     private static final Logger logger = LogManager.getLogger("HelloAction");
 
     @RequestMapping(value="/params2.action", method = RequestMethod.POST)
-    public String params2(Model model, String username, int[] hobby) throws Exception {
-        String str = "";
-        for (int i : hobby) {
-            str  = str + i + " ";
-        }
-        logger.info("params2 " + username + " " + str);
-        model.addAttribute("message", username + " " + str);
+    public String params2(String username, int[] hobby) throws Exception {
+        logger.debug(username);
         return "message";
     }
-
-
 
 }
