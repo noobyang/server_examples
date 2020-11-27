62364354b92d1c8c6f987900aaf88c0d8ed3e74b
liyang <1234567890>
2020-11-27 15:02:31 +0800

    lesson ssm: action assess, action log4j2 save file success, log path need absolute path <Property name="LOG_HOME">D:\logs</Property>

--- SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
+++ SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
@@ -20,7 +20,18 @@ public class HelloAction {
 
     @RequestMapping(value="/insert.action", method = RequestMethod.POST)
     public String insert(Dept dept) throws Exception {
-        logger.debug(dept.toString());
+        // 记录trace级别的信息
+        logger.trace("HelloAction log4j2日志输出：This is trace message.");
+        // 记录debug级别的信息
+        logger.debug("HelloAction log4j2日志输出：This is debug message.");
+        // 记录info级别的信息
+        logger.info("HelloAction log4j2日志输出：This is info message.");
+        // 记录error级别的信息
+        logger.error("HelloAction log4j2日志输出：This is error message.");
+        // 记录warn级别的信息
+        logger.warn("HelloAction log4j2日志输出：This is warn message.");
+
+        logger.debug("HelloAction " + dept.toString());
 
         deptDao.insertDept(dept);
 
