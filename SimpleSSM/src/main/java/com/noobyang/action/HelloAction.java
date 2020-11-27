dcb4bc10612b84498dc79c148cd61dafd25a5604
liyang <1234567890>
2020-11-27 10:02:41 +0800

    lesson ssm: action assess, operation database

--- SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
+++ SimpleSSM/src/main/java/com/noobyang/action/HelloAction.java
@@ -1,10 +1,13 @@
 package com.noobyang.action;
 
+import com.noobyang.dao.impl.DeptDaoImpl;
 import com.noobyang.entity.Dept;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping(value="/dept")
@@ -12,9 +15,15 @@ public class HelloAction {
 
     private static final Logger logger = LogManager.getLogger("HelloAction");
 
+    @Resource
+    private DeptDaoImpl deptDao;
+
     @RequestMapping(value="/insert.action", method = RequestMethod.POST)
     public String insert(Dept dept) throws Exception {
         logger.debug(dept.toString());
+
+        deptDao.insertDept(dept);
+
         return "message";
     }
 
