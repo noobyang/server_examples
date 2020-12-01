447228d4303292c3446a4791ee70bfe5a37d1383
liyang <1234567890>
2020-12-01 13:51:11 +0800

    lesson web: because Cross-domain problem, Combined Projects
    has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header...

--- SimpleSpringBoot2/src/main/java/com/noobyang/controller/HelloController.java
+++ SimpleSpringBoot2/src/main/java/com/noobyang/controller/HelloController.java
@@ -2,15 +2,22 @@ package com.noobyang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public @ResponseBody String hello() {
+    @RequestMapping(method = RequestMethod.GET, value = "/hello_get")
+    public @ResponseBody String helloGet() {
         System.out.println("欢迎进入 SpringBoot");
-        return "Hello World!";
+        return "Get Hello World!";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/hello_post")
+    public @ResponseBody String helloPost(String fname, String lname) {
+        System.out.println("欢迎进入 SpringBoot");
+        return "Post Hello World! " + fname + " " + lname;
     }
 
 }
