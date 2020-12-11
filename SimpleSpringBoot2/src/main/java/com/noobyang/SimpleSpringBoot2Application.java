7e5337bfa3d4f5ffe6e67ce89d7d0870f687bce1
liyang <1234567890>
2020-12-11 14:12:53 +0800

    lesson springboot2: build output war, deploy to tomcat

--- SimpleSpringBoot2/src/main/java/com/noobyang/SimpleSpringBoot2Application.java
+++ SimpleSpringBoot2/src/main/java/com/noobyang/SimpleSpringBoot2Application.java
@@ -3,13 +3,21 @@ package com.noobyang;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages={"com.noobyang.*"})
 @ComponentScan("com.noobyang.*") // 扫描所需要的包
 // 如果 ComponentScan 不覆盖 mapper 目录，可以使用下面注解单独配置 mapper 目录
 //@MapperScan(basePackages = {"com.noobyang.mapper"}, sqlSessionFactoryRef = "sqlSessionFactory")
-public class SimpleSpringBoot2Application {
+public class SimpleSpringBoot2Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(SimpleSpringBoot2Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleSpringBoot2Application.class, args);
